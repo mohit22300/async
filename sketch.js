f68cbe48986442a4100096d96bc1211ba93d3649
mohit <dugarmohit049@gmail.com>
2019-12-30 19:59:41 +0530

    updated sketdh.js

--- sketch.js
+++ sketch.js
@@ -1,29 +1,46 @@
-var ball;
+var hypnoticalball,database,position;
 
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    database=firebase.database();
+    hypnoticalball = createSprite(250,250,10,10);
+    hypnoticalball.shapeColor = "red";
+    var hypnoticalballposition=database.ref('ball/position');
+    hypnoticalballposition.on("value",readPosition,showError)
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref('ball/position').set({
+        'x':position.x+x,
+        'y':position.y+y,
+
+    })
+   
+}
+function readPosition(data)
+{
+    position=data.val();
+        hypnoticalball.x=position.x;
+        hypnoticalball.y=position.y;
+}
+function showError(){
+console.log("error in writing to the database");
+
 }
